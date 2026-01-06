8710b511dad66407f8d4b4f94b458bb918c3c543
Thyon3 <asnakemengesha80@gmail.com>
2026-01-05 19:29:05 -0500

    refactor: rebuild HomeScreen using Riverpod providers and domain entities

---
+++ lib/presentation/theme/text_styles.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static final splashScreenBigText = TextStyle(
+    fontSize: 35,
+    fontWeight: FontWeight.w700,
+    color: Colors.amber,
+  );
+  static final dullText = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    color: Colors.black,
+  );
+
+  static final buttonText = TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+}
